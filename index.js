cd3f1f6b2c77daa997b9cdbfb7c64c4900781cab
owenb132 <owenb132@users.noreply.github.com>
2016-04-17 10:13:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function sendTextMessage(sender, text) {
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'Hello world, I am a chat bot') {
+    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
