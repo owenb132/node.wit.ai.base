ace5564090c81bdf4c420035c8c001c7d33cf8d3
owenb132 <owenb132@users.noreply.github.com>
2016-04-17 09:00:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,9 +16,46 @@ app.get('/', function (req, res) {
     res.send('Hello world, I am a chat bot')
 })
 
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+var token = "<CAAZADZCMHhz70BAIOwOOpG9GhKRmrjNhFiRIGpU4gMZATOVKrD0ZBqveklLaYmz0YVqhQxlgirgl7SjevwXWPGHwm53lyHniuvhiAcrhkTSMTT8DwBMynuD772Qox2jlsgUrOUetsAcIx1ktRpEmYmuTD0cmNdBysT4NN2bfOwfDUlQZCAI7R8Nzx4EbOucZByu0N1C8sSwAZDZD>"
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'fantasy_football_coach') {
+    if (req.query['hub.verify_token'] === 'Hello world, I am a chat bot') {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
